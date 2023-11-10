11995311ada26fba9af480120e6e451d74a7d6d9
JohnProphet <87950318+JohnProphet@users.noreply.github.com>
2023-11-09 18:26:00 -0600

    roverr changes

--- rover.js
+++ rover.js
@@ -7,12 +7,15 @@ class Rover {
       this.wattage = wattage;
    }
    recieveMessage(message) {
+      let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+      let message = new Message('Test message with two commands', commands);
+      let rover = new Rover(98382);    // Passes 98382 as the rover's position.
       let response = {
          message : message.name,
          results : []
       }
       return response;
-   }
+      }
 
 }
 
